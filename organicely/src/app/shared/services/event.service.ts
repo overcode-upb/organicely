02f300a4349f7a6b6b71a59474e4a89e84b45fc5
Cesar Illanes <cesarill99@gmail.com>
2021-04-26 18:56:48 -0400

    Added Event and Upload service

---
+++ organicely/src/app/shared/services/event.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+  baseUrl = environment.app.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public createEvent(body: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/event.json`, body)
+  }
+
+  public getEvents(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/event.json`)
+  }
+
+  public getEventById(id: any): Observable<any> {
+    return this.http.get(`${this.baseUrl}/event/${id}.json`)
+  }
+
+  public updateEvent(id: any, body: any): Observable<any>{
+    return this.http.put(`${this.baseUrl}/event/${id}.json`, body)
+  }
+
+  public deleteEvent(id: any): Observable<any>{
+    return this.http.delete(`${this.baseUrl}/event/${id}.json`)
+  }
+}
