fffd46634a766674764d2723038571af7ad9c0b9
Camila <camila.loayzab@gmail.com>
2021-04-27 00:56:01 -0400

    Changes to profile

--- organicely/src/app/pages/profile/profile.component.ts
+++ organicely/src/app/pages/profile/profile.component.ts
@@ -9,6 +9,33 @@ export class ProfileComponent implements OnInit {
 
   constructor() { }
 
+  events =  [
+      {
+        "nombre": "Concierto",
+        "horario": "12:00",
+        "urlImage": "https://www.weddingsutra.com/images/Vendor_Images/Wedding_Planners/Version-Events-and-Weddings/version-events-weddings-01.jpg",
+        "descripcion": "The best football team in the world!",
+        "urlInfo": "lol"
+      },
+      {
+        "nombre": "Fiesta",
+        "horario": "17:00",
+        "urlImage": "https://www.parishrenewal.com.au/wp-content/uploads/2019/04/Events.jpg",
+        "descripcion": "The best football team in the world!",
+        "urlInfo": "lol"
+      }
+    ];
+
+  pastevents =  [
+      {
+        "nombre": "Concierto",
+        "horario": "12:00",
+        "urlImage": "https://miro.medium.com/max/720/0*SkLaMDVqEL3WLtAl",
+        "descripcion": "The best football team in the world!",
+        "urlInfo": "lol"
+      },
+    ]
+
   ngOnInit(): void {
   }
 
