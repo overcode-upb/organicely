1ed1788d7adca5ebfa3c3bace9f7c4538399dc3d
Camila <camila.loayzab@gmail.com>
2021-05-12 21:10:25 -0400

    Deleting firebase functions

--- functions/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redirectOauth = void 0;
-const functions = require("firebase-functions");
-exports.redirectOauth = functions.https.onRequest((req, res) => {
-    res.redirect("https://www.google.com/");
-});
-//# sourceMappingURL=index.js.map
